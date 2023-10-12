7ef1ea9982944107f27572c11c23f5d8a97daa8e
Estian Yssel <estianyssel@gmail.com>
2023-10-12 21:40:44 +0200

    feat: initial constructs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Queue, QueueProps } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 
+export interface MonitoredQueueProps extends Omit<QueueProps, 'deadLetterQueue'> {
+  maxReceiveCount?: number;
+  dlqQueueName?: string;
+}
+
 export class MonitoredQueue extends Queue {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: MonitoredQueueProps) {
     super(scope, id);
+
+    const { maxReceiveCount, dlqQueueName } = props;
+
+    const dlq = new Queue(scope, 'DLQ', {
+      queueName: dlqQueueName || `${this.queueName}-dlq`,
+    });
+
+    new Queue(scope, id, {
+      ...props,
+      deadLetterQueue: {
+        maxReceiveCount: maxReceiveCount || 5,
+        queue: dlq,
+      },
+    });
   }
 }
