3adc0849adb8f49db60b0e184bdfa46b6c4de6cf
Estian Yssel <estianyssel@gmail.com>
2023-10-23 08:18:55 +0200

    chore: formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Alarm } from "aws-cdk-lib/aws-cloudwatch";
-import { Queue, QueueProps } from "aws-cdk-lib/aws-sqs";
-import { Construct } from "constructs";
+import { Alarm } from 'aws-cdk-lib/aws-cloudwatch';
+import { Queue, QueueProps } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
 
 export interface MonitoredQueueProps {
   /** The properties of the SQS Queue Construct */
@@ -16,18 +16,18 @@ export class MonitoredQueue extends Construct {
     const deadLetterQueue = props.queueProps.deadLetterQueue
       ? props.queueProps.deadLetterQueue
       : {
-          queue: new Queue(this, "DeadLetterQueue", {
-            queueName: `${props.queueProps.queueName}-dlq`,
-          }),
-          maxReceiveCount: 3,
-        };
+        queue: new Queue(this, 'DeadLetterQueue', {
+          queueName: `${props.queueProps.queueName}-dlq`,
+        }),
+        maxReceiveCount: 3,
+      };
 
-    new Queue(this, "Queue", {
+    new Queue(this, 'Queue', {
       ...props.queueProps,
       deadLetterQueue,
     });
 
-    new Alarm(this, "DLQ-Alarm", {
+    new Alarm(this, 'DLQ-Alarm', {
       metric: deadLetterQueue.queue.metricApproximateNumberOfMessagesVisible(),
       threshold: props.messageThreshold || 5,
       evaluationPeriods: 1,
