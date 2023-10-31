d04f9c9f45a1e066fd920a6d60cab33ef34e852c
Estian Yssel <estianyssel@gmail.com>
2023-10-31 06:54:15 +0200

    fix: reintroduce esbuild with a custom script

---
+++ scripts/buildLambdas.ts
@@ -0,0 +1,28 @@
+import * as esbuild from "esbuild";
+import * as path from "path";
+
+const lambdaPaths = ["lambda/slackListener/"];
+
+async function buildLambdas() {
+  for (const lambdaPath of lambdaPaths) {
+    let pathTs = path.join(`src/${lambdaPath}`, "index.ts");
+    let pathJs = path.join(`lib/${lambdaPath}`, "index.js");
+
+    await esbuild.build({
+      platform: "node",
+      target: ["esnext"],
+      minify: true,
+      bundle: true,
+      keepNames: true,
+      sourcemap: "linked",
+      sourcesContent: false,
+      entryPoints: [pathTs],
+      outfile: pathJs,
+      external: [""],
+      logLevel: "warning",
+      metafile: true,
+    });
+  }
+}
+
+buildLambdas();
