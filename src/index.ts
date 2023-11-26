04b76d99a44c33655281ad0550d9799195402d5e
Estian Yssel <estianyssel@gmail.com>
2023-11-26 19:50:41 +0200

    feat: adds finite lambda log retention

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as path from 'path';
 import { Alarm, TreatMissingData } from 'aws-cdk-lib/aws-cloudwatch';
 import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
@@ -9,6 +8,7 @@ import {
 } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Queue, QueueProps } from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as path from 'path';
 
 export interface IMonitoredQueueProps {
   /** The properties of the SQS Queue Construct */
@@ -91,6 +91,7 @@ export class MonitoredQueue extends Construct {
         SLACK_BOT_TOKEN: slackToken,
         SLACK_CHANNEL: slackChannel,
       },
+      logRetention: 7,
     });
 
     topic.addSubscription(new LambdaSubscription(slackListener));
