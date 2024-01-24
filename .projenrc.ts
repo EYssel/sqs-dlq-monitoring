1b3a96cc717fa2b38d3f769ea5c10cd15b49b7f4
Estian Yssel <estianyssel@gmail.com>
2024-01-24 11:20:46 +0200

    feat: reduce minimum cdk version

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Estian Yssel',
   authorAddress: 'estianyssel@gmail.com',
-  cdkVersion: '2.121.1',
+  cdkVersion: '2.60.0',
   defaultReleaseBranch: 'master',
   jsiiVersion: '~5.3.8',
   name: 'sqs-dlq-monitoring',
@@ -14,12 +14,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
       testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
     },
   },
-  // deps: [],                    /* Runtime dependencies of this module. */,
-  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: [
     'esbuild',
-  ] /* Build dependencies for this module. */,
-  // packageName: undefined,      /* The "name" in package.json. */
+  ],
   bundledDeps: ['axios'],
   // exclude lambda code in favour of custom esbuild compiling
   excludeTypescript: ['src/lambda/**/*', 'playground/**/*'],
