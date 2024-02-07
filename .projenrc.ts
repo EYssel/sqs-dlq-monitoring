287c15d81da56b5f8277683717b52a5bf4ce1811
Estian Yssel <estianyssel@gmail.com>
2024-02-07 16:03:35 +0200

    chore: bump to v1

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,8 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Estian Yssel',
   authorAddress: 'estianyssel@gmail.com',
-  description: 'An AWS CDK construct which creates an AWS Simple-Queue Service (SQS) queue with an appropriately monitored Dead-Letter Queue (DLQ). This so called MonitoredQueue construct will send messages to the specified locations to notify you if messages in the DLQ cross a certain threshold',
+  description:
+    'An AWS CDK construct which creates an AWS Simple-Queue Service (SQS) queue with an appropriately monitored Dead-Letter Queue (DLQ). This so called MonitoredQueue construct will send messages to the specified locations to notify you if messages in the DLQ cross a certain threshold',
   cdkVersion: '2.60.0',
   defaultReleaseBranch: 'master',
   jsiiVersion: '~5.3.8',
@@ -15,12 +16,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
       testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
     },
   },
-  devDeps: [
-    'esbuild',
-  ],
+  devDeps: ['esbuild'],
   bundledDeps: ['axios'],
   // exclude lambda code in favour of custom esbuild compiling
   excludeTypescript: ['src/lambda/**/*', 'playground/**/*'],
+  majorVersion: 1,
 });
 project.npmignore!.exclude('scripts/');
 project.npmignore!.exclude('playground/');
