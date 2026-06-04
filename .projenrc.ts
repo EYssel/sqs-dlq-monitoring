9321e0da7dea111f6dbb674f800ce84859a2d2e1
Estian Yssel <estian.yssel@smartvokat.com>
2026-06-04 12:34:50 +0200

    fix: projenr cdk major version

--- .projenrc.ts
+++ .projenrc.ts
@@ -21,7 +21,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   bundledDeps: ['axios'],
   // exclude lambda code in favour of custom esbuild compiling
   excludeTypescript: ['src/lambda/**/*', 'playground/**/*'],
-  majorVersion: 1,
+  majorVersion: 2,
 });
 project.npmignore!.exclude('scripts/');
 project.npmignore!.exclude('playground/');
